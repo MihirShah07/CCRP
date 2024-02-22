9835372eda07892dfbfe17da6c94b5b428c82269
Mihir Shah <7mihirpshah@gmail.com>
2024-02-23 00:16:55 +0530

    first commit

---
+++ static/add_complaint/script.js
@@ -0,0 +1,20 @@
+let caseIdCounter = 1;
+let startID = 1000001;
+function createNewCase() {
+  // Increment the case ID counter
+  let newCaseID = startID + caseIdCounter;
+    console.log(newCaseID);
+  // Update the case ID input value
+  const caseIdInput = document.getElementById('caseId');
+  caseIdInput.value = `${newCaseID}`;
+}
+  function getCurrentDateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  }
